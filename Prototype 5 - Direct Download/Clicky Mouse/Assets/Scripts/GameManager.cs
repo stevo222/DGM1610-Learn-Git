45ba981f6568e50c34dac1322443ca832421b826
stevo222 <stephenostler@yahoo.com>
2022-04-11 13:25:24 -0600

    Click-o-Rama game

---
+++ Prototype 5 - Direct Download/Clicky Mouse/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titlescreen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+            
+        }
+
+    }
+
+    public void UpdateScore (int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+
+        isGameActive = true;
+        score = 0;
+        spawnRate = spawnRate / difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titlescreen.gameObject.SetActive(false);
+    }
+}
